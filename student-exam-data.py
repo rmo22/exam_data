2e4f28e48b80c53439145a5443a7d787a59d3ac6
rmo22 <morecroftr@gmail.com>
2021-12-11 21:32:24 +0000

    import data to variable

--- student-exam-data.py
+++ student-exam-data.py
@@ -2,3 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+StudentPerformanceData = pd.read_csv('StudentsPerformance.csv')
+print(StudentPerformanceData)
+
+## Questions
+# 1. What was average scores across all students? 
+# 2. What was the average scores of males vs females?
+# 3. What was the average scores by ethnicity? 
+# 4. What was the average scores by level of education? 
+# 5. What was the average scores by socio-economics? 
+# 6. What is the biggest factor to affect grades
